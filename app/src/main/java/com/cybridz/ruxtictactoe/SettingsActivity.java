c6d068f272a9c8bb7625d5c9c3cb9e52468739d9
benjamin <benjamin.w@cybridz.com>
2024-06-17 01:12:41 +0200

    Setting page

---
+++ app/src/main/java/com/cybridz/ruxtictactoe/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.cybridz.ruxtictactoe;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.cybridz.AbstractActivity;
+
+public class SettingsActivity extends AbstractActivity {
+
+    /**
+     * Settings Activity elements
+     */
+    @SuppressWarnings("FieldCanBeLocal")
+    private Button start_button;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        current_view = findViewById(R.id.start_activity);
+        initializeServices();
+        start_button = findViewById(R.id.start_game);
+        start_button.setText("Start");
+        start_button.setOnClickListener(view -> goToGameActivity());
+    }
+
+    public void goToGameActivity(){
+        sharedServices.getBlinkingLightMessageService().stop();
+        startActivity(new Intent(SettingsActivity.this, GameActivity.class));
+    }
+
+    @Override
+    public void play() {
+        if (sharedServices.isOpenedServices()){
+            sharedServices.getBlinkingLightMessageService().setRandomEarColor();
+            sharedServices.getBlinkingLightMessageService().start();
+            sharedServices.getRobotService().robotPlayTTs("Let's play Tic Tac Toe");
+        }
+        Log.d(LOGGER_KEY, "playing start activity");
+    }
+}
